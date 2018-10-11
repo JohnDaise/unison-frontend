6946652c1b30dcd77dd2532b239c866042ead414
JohnDaise <jdaise@gmail.com>
2018-10-11 15:39:23 -0400

    semantic ui reat added

---
+++ src/components/login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from 'semantic-ui-react'
+
+
+class Login extends React.Component {
+
+
+
+  render(){
+    return(
+      <React.Fragment>
+        <h1> Unison </h1>
+        <Button> Login with Gmail </Button>
+      </React.Fragment>
+    )
+  }
+
+}
+
+
+export default Login;
